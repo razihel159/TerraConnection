d145355e2e640a116b8aef717c44677e43e0e926
Mark Gelo Rioflorido <markgelorioflorido@gmail.com>
2025-02-04 20:49:45 +0800

    error2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,7 @@ dependencies {
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 }
